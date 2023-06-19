6078201a2c8527e84d30cde81aed9591191615f8
Carlo Dee <carlodee.official@proton.me>
2023-06-19 21:31:59 +0800

    golf: device tree update. * Create release tool extension. * Removed charger_fstab.qti, it is already part of vendor tree. * Clean up.
    Signed-off-by: Carlo Dee <carlodee.official@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,14 +8,14 @@ DEVICE_PATH := device/realme/golf
 
 # Architecture
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-a
+TARGET_ARCH_VARIANT := armv8-2a-dotprod
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 := 
 TARGET_CPU_VARIANT := generic
 TARGET_CPU_VARIANT_RUNTIME := cortex-a9
 
 TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_ARCH_VARIANT := armv8-2a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
@@ -114,7 +114,8 @@ BOARD_KERNEL_CMDLINE += \
     androidboot.hardware=qcom \
     androidboot.console=ttyMSM0 \
     androidboot.memcg=1 \
-    androidboot.selinux=permissive
+    androidboot.selinux=permissive \
+    androidboot.init_fatal_reboot_target=recovery
  
 # Kernel (Prebuilt)
 TARGET_FORCE_PREBUILT_KERNEL := true
@@ -150,6 +151,9 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.emmc
 TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
+
+# Release Tools
+TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 
 # Security patch level
 VENDOR_SECURITY_PATCH := 2023-02-05
