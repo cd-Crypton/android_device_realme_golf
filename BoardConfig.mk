0a476dce049a481b9e6b0637d3710f1ee97b38d4
Carlo Dee <carlodee.official@proton.me>
2023-06-20 09:29:27 +0800

    golf: initial sepolicy commits from hardware/oplus [Dirty Commit]. Ref: https://github.com/LineageOS/android_hardware_oplus.git
    Signed-off-by: Carlo Dee <carlodee.official@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -169,7 +169,9 @@ USE_SENSOR_MULTI_HAL := true
 
 # SEPolicy
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
-include hardware/oplus/sepolicy/qti/SEPolicy.mk
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 
 # Touch
 SOONG_CONFIG_NAMESPACES += OPLUS_LINEAGE_TOUCH_HAL
